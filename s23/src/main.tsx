de5554a059c4f566967db836fe2b4566ff9b1c7d
Javid Izadfar <javid.izadfar@gmail.com>
2026-07-28 19:52:24 +0430

    cleanup root dir

--- s23/src/main.tsx
+++ s23/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./assets/styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
